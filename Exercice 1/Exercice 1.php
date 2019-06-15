f9b738dc53c0a2f4caf91b22d0a5a8117a7d731a
imed jaberi <imed_jebari@hotmail.fr>
2019-06-15 21:12:40 +0200

    correction of the exercice 1 ..

---
+++ Exercice 1/Exercice 1.php
@@ -0,0 +1,33 @@
+<?php
+  
+  function calcul_somme ($tab) {
+
+    $SP = 0 ;
+    $SN = 0 ;
+
+    foreach ($tab as $item) {
+
+        if( gettype($item) != "integer" && gettype($item) != "double" ){
+          echo (" Le tableau contient une valeur non numérique ! ");
+        }else{
+          if($item >= 0){
+            $SP += $item ;
+          }else{
+            $SN += $item ;
+          }
+        }
+
+    }
+
+    echo (" Le somme des nombres positives : $SP ");
+    echo (" Le somme des nombres negatives : $SN ");
+  
+  }
+
+  $TABLEAU = array(10,-5,2,3,-6) ; 
+  
+  print_r($TABLEAU);
+
+  calcul_somme($TABLEAU);
+
+?>
